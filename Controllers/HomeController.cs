b9c5989ddbc0a611d2810de7b8ba46a971ba7f34
thomas-oliver-petty <thomas.o.petty@btinternet.com>
2026-03-24 12:19:36 +0000

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using spireHelper.Models;
+
+namespace spireHelper.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IWebHostEnvironment _env;
+
+    public HomeController(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public IActionResult Index()
+    {
+        var cardsFolder = Path.Combine(_env.WebRootPath, "images", "cards");
+        var cardUrls = new List<string>();
+
+        if (Directory.Exists(cardsFolder))
+        {
+            cardUrls = Directory.EnumerateFiles(cardsFolder, "*.png")
+                .Where(f => !Path.GetFileName(f).Contains("Plus", StringComparison.OrdinalIgnoreCase))
+                .OrderBy(f => Path.GetFileName(f))
+                .Select(f => $"/images/cards/{Path.GetFileName(f)}")
+                .ToList();
+        }
+
+        return View(cardUrls);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
